f56dcf6e65ddb0fc920e4224fe4a078f450f3abd
liyx <yuxing.li@chemical.ai>
2025-05-21 17:58:43 +0800

    新增注册登录

--- api/function/varifyVip_view.py
+++ api/function/varifyVip_view.py
@@ -13,35 +13,16 @@ from api.selfUtils import rsa_decrypt, result
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 class varifyVip(TokenObtainPairView):
-    def post(self, request, *args, **kwargs):
-        # 1. 从请求中获取前端加密的用户名/密码
-        encrypted_data = request.data.get('data')
-
-        # 2. 后端解密获取原始凭证
-        try:
-            decrypted_data = rsa_decrypt(encrypted_data)
-            decrypted_data = json.loads(decrypted_data)
-            username = decrypted_data.get("username")
-            password = decrypted_data.get("password")
-            timestamp = decrypted_data.get("timestamp")
-            print(f"解密后的用户名: {username}, 密码: {password}, 时间戳: {timestamp}")
-        except Exception as e:
-            return result.fail('无效的加密数据', code=status.HTTP_400_BAD_REQUEST)
-
-        # 3. 验证用户凭证
-        user = authenticate(username=username, password=password)
-        if not user:
-            return result.fail("用户名或密码错误", code=status.HTTP_401_UNAUTHORIZED)
-
-        # 4. 生成 JWT（使用 simplejwt 的 RefreshToken 类）
-        try:
-            refresh = RefreshToken.for_user(user)
-            response_data = {
-                'refresh': str(refresh),
-                'access': str(refresh.access_token)
-            }
-        except Exception as e:
-            return result.fail("生成令牌失败", code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        # 1. 获取用户的 VIP 到期时间和设备号
+        vip_time = user.vip_time
+        device = user.device
+        ip_address = user.ip_address
+        data = {
+            'vip_time': vip_time,
+            'device': device,
+            'ip_address': ip_address
+        }
         # 5. 返回成功响应
-        return result.success(response_data)
+        return result.success(data)
